2b6f72b00a5173ce39b463242e3fd5c29cf62619
Timin Alexander <timin.1996@list.ru>
2023-04-18 20:13:34 +0300

    Add task2

--- Synchronized/DeliveryRobot/src/Main.java
+++ Synchronized/DeliveryRobot/src/Main.java
@@ -2,11 +2,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
         Map<Integer, Integer> sizeToFreq = new TreeMap<>();
         List<Thread> threads = new ArrayList<>();
 
-        for (int i = 0; i < 1000; i++) {
-            Thread thread = new Thread(() -> {
+        Thread threadPrinter = new Thread(() -> {
+            synchronized (sizeToFreq) {
+                Map.Entry<Integer, Integer> max;
+                while (!Thread.interrupted()) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+
+                    }
+                    max = findMax(sizeToFreq);
+                    System.out.printf("Повторений %s (встретилось %s раз)\n", max.getKey(), max.getValue());
+                }
+            }
+        });
+        threadPrinter.start();
+
+        for (int i = 0; i < 100; i++) {
+            Thread threadCal = new Thread(() -> {
                 int repeats = 0;
                 String route = generateRoute("RLRFR", 100);
                 for (int j = 0; j < route.length(); j++) {
@@ -22,14 +39,18 @@ public class Main {
                     } else {
                         sizeToFreq.put(repeats, 1);
                     }
+                    sizeToFreq.notify();
                 }
             });
-            threads.add(thread);
-            thread.start();
+            threads.add(threadCal);
+            threadCal.start();
         }
-        for (Thread thread: threads) {
+
+        for (Thread thread : threads) {
             thread.join();
         }
+
+        threadPrinter.interrupt();
         printResult(sizeToFreq);
     }
 
@@ -43,19 +64,21 @@ public class Main {
     }
 
     public static void printResult(Map<Integer, Integer> result) {
-        Map.Entry<Integer, Integer> max = result.entrySet()
-                .stream()
-                .max(Map.Entry.comparingByValue())
-                .get();
-
-        result.remove(max.getKey());
+        Map.Entry<Integer, Integer> max = findMax(result);
 
         System.out.printf("Самое частое количество повторений %s (встретилось %s раз)\n", max.getKey(), max.getValue());
         System.out.println("Другие размеры:");
 
         result.entrySet()
                 .stream()
-                .sorted(Comparator.comparingInt(e -> - e.getValue()))
+                .sorted(Comparator.comparingInt(e -> -e.getValue()))
                 .forEach(e -> System.out.println(" - " + e.getKey() + " (" + e.getValue() + " раз)"));
+    }
+
+    private static Map.Entry<Integer, Integer> findMax(Map<Integer, Integer> result) {
+        return result.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
     }
 }
