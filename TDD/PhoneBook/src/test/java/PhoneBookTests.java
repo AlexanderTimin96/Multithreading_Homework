c64c9082330e012a684dd264eb4f1fecd62deda2
Timin Alexander <timin.1996@list.ru>
2023-05-03 21:36:51 +0300

    Stub and tests for add

--- TDD/PhoneBook/src/test/java/PhoneBookTests.java
+++ TDD/PhoneBook/src/test/java/PhoneBookTests.java
@@ -1,2 +1,45 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.ntology.PhoneBook;
+
+import java.util.Random;
+
 public class PhoneBookTests {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void beforeEachTests() {
+        phoneBook = new PhoneBook();
+    }
+
+    @AfterEach
+    public void afterEachTests() {
+        phoneBook = null;
+    }
+
+    @Test
+    public void TestAdd_WithReplay() {
+        int excepted = 1;
+
+        int result = 0;
+        for (int i = 0; i < 3; i++) {
+            result = phoneBook.add("name", "nubmer");
+        }
+
+        Assertions.assertEquals(excepted, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 5, 8})
+    public void TestAdd_SimpleAdd(int argument) {
+        int excepted = argument;
+
+        int result = 0;
+        for (int i = 0; i < argument; i++) {
+            result = phoneBook.add("name" + argument, "nubmer");
+        }
+
+        Assertions.assertEquals(excepted, result);
+    }
 }
